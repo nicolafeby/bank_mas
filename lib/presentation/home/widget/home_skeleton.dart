3883b86e1352d74e5d8777f5b5f76bc60230bf43
Nicola <nicolafsalv@gmail.com>
2023-10-22 14:40:25 +0700

    feature: create article skeleton

--- lib/presentation/home/widget/home_skeleton.dart
+++ lib/presentation/home/widget/home_skeleton.dart
@@ -7,64 +7,126 @@ class HomeSkeleton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 8.h),
+        _buildMenuSkeleton(),
+        SizedBox(height: 12.h),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12.w),
+          child: Shimmer.fromColors(
+            baseColor: Colors.grey[300]!,
+            highlightColor: Colors.grey[100]!,
+            child: Container(
+              height: 16.h,
+              width: 55.w,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(4.r))),
+            ),
+          ),
+        ),
+        SizedBox(height: 4.h),
+        Expanded(
+          child: ListView.separated(
+            padding: EdgeInsets.all(8.r),
+            itemBuilder: (context, index) => _buildArticleSkeleton(),
+            separatorBuilder: (context, index) => SizedBox(height: 8.h),
+            itemCount: 5,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildArticleSkeleton() {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.r),
+      ),
+      elevation: 4,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          _buildMenuSkeleton(),
-          // Expanded(
-          //   child: ListView.builder(
-          //     itemBuilder: (context, index) {
-          //       return Container();
-          //     },
-          //   ),
-          // ),
+          Shimmer.fromColors(
+            baseColor: Colors.grey[300]!,
+            highlightColor: Colors.grey[100]!,
+            child: Container(
+              height: 85.h,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(8.r),
+                    topRight: Radius.circular(8.r)),
+              ),
+            ),
+          ),
+          // SizedBox(height: 35.h)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Shimmer.fromColors(
+              baseColor: Colors.grey[300]!,
+              highlightColor: Colors.grey[100]!,
+              child: Container(
+                height: 16.h,
+                width: 0.7.sw,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(4.r))),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
   Widget _buildMenuSkeleton() {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.r),
-      ),
-      elevation: 4,
-      child: Shimmer.fromColors(
-        baseColor: Colors.grey[300]!,
-        highlightColor: Colors.grey[100]!,
-        child: Padding(
-          padding: EdgeInsets.all(8.r),
-          child: GridView.count(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            crossAxisCount: 3,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            children: List.generate(
-              5,
-              (index) => Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 55.r,
-                    width: 55.r,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.r),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.w),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.r),
+        ),
+        elevation: 4,
+        child: Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: Padding(
+            padding: EdgeInsets.all(8.r),
+            child: GridView.count(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 3,
+              padding: const EdgeInsets.all(20),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              children: List.generate(
+                5,
+                (index) => Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 55.r,
+                      width: 55.r,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.r),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 8.h),
-                  Container(
-                    height: 16.h,
-                    width: 65.w,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(4.r),
+                    SizedBox(height: 8.h),
+                    Container(
+                      height: 16.h,
+                      width: 65.w,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(4.r),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
